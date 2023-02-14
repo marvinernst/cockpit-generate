a263d74ac1affde87d1322735376eeea9a2f1cd3
Marvin Ernst <marivn.ernst89@gmail.com>
2023-02-14 17:22:44 +0100

    first commit

---
+++ bin/index.js
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv')
+
+
+dotenv.config({ path: '../../.env' });
+
+const API_KEY = process.env.API_KEY;
+const API_BASE_URL = process.env.API_BASE_URL;
+
+const routes = require('../../../cockpit.config.cjs');
+
+const client = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'api-key': `${API_KEY}`
+  }
+});
+
+
+const generateModule = async (routes, client) => {
+
+  let fileData = `
+  import axios from 'axios';
+  const getAll = require("./filter");
+  const api = axios.create({
+    baseURL: '${API_BASE_URL}s',
+    headers: {
+      'api-key': '${API_KEY}'
+    }
+  });
+
+  `;
+  const promises  =  routes.map( (route) => {
+    return client.get(route.path);
+  })
+  const responses = await Promise.all(promises);
+  const types = responses.map( (response, index) => {
+    const types = jsonToTypeScript(response.data)
+    const typeName = capitalizeFirstLetter(routes[index].name)
+    return  `type ${typeName}=${types};\n\n`
+  })
+
+  const typesFilter = responses.map( (response, index) => {
+    const types = jsonToTypeScript(response.data, true)
+    const typeName = capitalizeFirstLetter(routes[index].name)
+    return  `type ${typeName}Filter={
+      where:${types}
+    };\n\n`
+  })
+
+
+  const functions =  routes.map((route) => {
+    const typeName = capitalizeFirstLetter(route.name)
+    const wuffel = `
+        ${route.name} : {
+          getAll: async (filter?:${typeName}Filter) => {
+            if (filter) {
+              const { data } = await api.get<${typeName}[]>("stationen");
+              return getAll(data, filter);
+            }
+              const {data} = await api.get<${typeName}[]>('${route.path}');
+              return data;
+          },
+          get: async (id:string) => {
+            const {data} = await api.get<${typeName}[]>('${route.path}/'+id);
+            return data;
+          }
+        },
+    `;
+
+    return wuffel;
+  }, {});
+  fileData += types.join(' ');
+  fileData += typesFilter.join(' ');
+  fileData += `const client = {`
+  fileData += functions.join(' ');
+  fileData += `}`
+  fileData +=   `
+    export default client;
+  `;
+  return fileData;
+};
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function jsonToTypeScript(json, optional) {
+  let result = '';
+  if(json === null) {
+    return
+  }
+  if (Array.isArray(json)) {
+    const type = typeof json[0];
+    result += `Array<${type === 'object' ? '{' : ''}${type}${type === 'object' ? '}' : ''}>`;
+  } else if (typeof json === 'object') {
+
+    result += '{\n';
+    Object?.keys(json)?.forEach(key => {
+      result += `  ${optional? key+'?': key}: ${jsonToTypeScript(json[key],optional)};\n`;
+    });
+    result += '}';
+  } else {
+    result += typeof json;
+  }
+  return result;
+}
+
+
+
+const baum = async () => {
+  const data = await generateModule(routes, client);
+  const filePath = path.join(__dirname, '../client.ts');
+  fs.writeFileSync(filePath, data);
+  console.log(`Generated API module at ${filePath}`);
+}
+
+baum();
+
---
+++ filter.ts
@@ -0,0 +1,31 @@
+module.exports = function getAll(data: any, options: any) {
+  const { where } = options;
+  const filteredData = data.filter((item: any) => {
+    // Iterate over the properties in the where object
+    for (const prop in where) {
+      if (where.hasOwnProperty(prop)) {
+        // Check if the property is a nested object
+        if (typeof where[prop] === "object") {
+          for (const nestedProp in where[prop]) {
+            if (where[prop].hasOwnProperty(nestedProp)) {
+              // Check if the nested property matches the item value
+              if (
+                item[prop] &&
+                item[prop][nestedProp] !== where[prop][nestedProp]
+              ) {
+                return false;
+              }
+            }
+          }
+        } else {
+          // Check if the property matches the item value
+          if (item[prop] !== where[prop]) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  });
+  return filteredData;
+};
